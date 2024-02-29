ddcc763243313efab228210e413a74b0aadc6913
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-02-29 17:59:12 -0500

    requete complete

--- requetes Bettina.sql
+++ requetes Bettina.sql
@@ -10,7 +10,7 @@
 
 SELECT avatar AS "Avatar", 
 	'[' || habilete.sigle || ' - ' || habilete.nom || ']' AS "Sigle et nom",
-	liste_av.date_obtention AS "Date dobtention",
+	liste_av.date_obtention AS "Date d''obtention",
 	liste_av.niveau_actuel AS "Niveau actuel",
 	((liste_coef.coef1*liste_av.niveau_actuel*liste_av.niveau_actuel)
 	+ (liste_coef.coef2*liste_av.niveau_actuel) + liste_coef.coef3) AS "moX du niveau courant",
@@ -20,34 +20,22 @@ SELECT avatar AS "Avatar",
 	ON liste_av.habilete = habilete.nom
 	INNER JOIN liste_coef_habilete AS liste_coef
 	ON liste_coef.habilete = habilete.nom
-		 WHERE avatar LIKE '%*'                      
-		
+		 WHERE avatar LIKE '%*';   
+		 
+		 
+--- REQUETE 6 ---
+-- ENONCÉ: Donnez la liste de tous les avatars qui possèdent plus de 1 item : nom du joueur,
+--nom de l’avatar et nombre d’items.
+--AUTEUR: BETTINA
+--Fonctionelle: OUI
 
-
--- INSERT INTO avatar (nom, date_creation, moX)
--- 	VALUES	('Jacque Bling*', CURRENT_DATE, 3),
--- 			('Time Minister', CURRENT_DATE, 9000000);
-
--- INSERT INTO habilete(nom, sigle, energie_maximum, description)
--- 	VALUES	('Visée automatique', 'SF1', 500.500, 'Améliore grandement la précision'),
--- 			('Ubiquité', 'SF2', 900.000, 'Améliore la durée de la multiprésence');
-			
--- INSERT INTO liste_coef_habilete (id, coef1, coef2, coef3, habilete)
--- 	VALUES	(1112222, 0.5, 1.5, 2.0, 'Visée automatique'),
--- 	      	(1113333, 0.5, 1.5, 2.0, 'Ubiquité');
-
--- INSERT INTO liste_habilete_avatar (id, date_obtention, niveau_actuel, habilete, avatar)
--- 	VALUES	(1112222, CURRENT_TIMESTAMP, 15,'Visée automatique', 'Jacque Bling*'),
--- 			(1113333, CURRENT_TIMESTAMP, 5,'Ubiquité','Jacque Bling*'),
--- 			(2224444, CURRENT_TIMESTAMP, 1,'Visée automatique', 'Time Minister'),
--- 			(2225555, CURRENT_TIMESTAMP, 1,'Ubiquité','Time Minister');
-
-		
--- SELECT  nom AS "Nom", 
---         prenom AS "Prénom", 
---         TO_CHAR(date_embauche, 'DD/MM/YYYY') AS "Date d'embauche", 
---         departement AS "Numéro de département"
---     FROM employe
---     WHERE superviseur = (SELECT nas FROM employe WHERE nom = 'Dupuis' AND prenom = 'Lancelot')
---     ORDER BY date_embauche DESC;
-		
+SELECT jo.alias AS "Joueur", liste_av.avatar AS "Avatar", liste_it.quantite AS "Nombre d''items",
+liste_it.item AS "Item"
+FROM liste_avatar_joueur AS liste_av
+INNER JOIN joueur AS jo
+ON liste_av.joueur = jo.alias
+INNER JOIN avatar AS av
+ON liste_av.avatar = av.nom
+INNER JOIN liste_item_avatar AS liste_it
+ON av.nom = liste_it.avatar
+WHERE liste_it.quantite > 1;
